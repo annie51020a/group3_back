2f36fe17dabd809a808703611725f4b727b93ee7
christ1113 <class30831@gmail.com>
2024-06-15 15:49:23 +0800

    add_iview

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import './assets/sass/_style.scss'
 
 import { createApp } from 'vue'
+import ViewUIPlus from 'view-ui-plus'
+
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'view-ui-plus/dist/styles/viewuiplus.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ViewUIPlus)
+
 
 app.mount('#app')
